998fa796e3c062f8a245defd289efb8c7f1fe303
hejialiang <984714129@qq.com>
2019-11-19 22:12:02 +0800

    修改名称
    Signed-off-by: hejialiang <984714129@qq.com>

--- build/copy.js
+++ build/copy.js
@@ -2,5 +2,5 @@ require('shelljs/global')
 
 rm('-rf', './public/admin/')
 mkdir('-p', './public/admin/')
-cp('-R', 'favicon.ico', './public/')
+// cp('-R', 'favicon.ico', './public/')
 cp('-R', 'robots.txt', './public/')
